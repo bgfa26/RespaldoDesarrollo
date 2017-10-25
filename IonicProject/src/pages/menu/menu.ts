eff3f2243ad2c59fbec83e3625c9c8dd0909c234
bgfa26 <bgfa26@gmail.com>
2017-10-25 01:09:19 -0400

    Subida Proyecto Desarrollo

---
+++ IonicProject/src/pages/menu/menu.ts
@@ -0,0 +1,74 @@
+import { InicioPage } from './../inicio/inicio';
+import {TendenciasPage } from './../tendencias/tendencias';
+import {SuscripcionesPage} from './../suscripciones/suscripciones';
+import {RecomendadosPage} from './../recomendados/recomendados';
+import { TabsPage } from './../tabs/tabs';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, Nav } from 'ionic-angular';
+ 
+export interface PageInterface {
+  title: string;
+  pageName: string;
+  tabComponent?: any;
+  index?: number;
+  icon: string;
+}
+ 
+@IonicPage()
+@Component({
+  selector: 'page-menu',
+  templateUrl: 'menu.html',
+})
+export class MenuPage {
+  // Basic root for our content view
+  rootPage = 'TabsPage';
+ 
+  // Reference to the app's root nav
+  @ViewChild(Nav) nav: Nav;
+ 
+  pages: PageInterface[] = [
+    { title: 'Inicio', pageName: 'TabsPage', tabComponent: 'InicioPage', index: 0, icon: 'home' },
+    { title: 'Tendencias', pageName: 'TabsPage', tabComponent: 'TendenciasPage', index: 1, icon: 'contacts' },
+    { title: 'Suscripciones', pageName: 'TabsPage',tabComponent: 'SuscripcionesPage',index: 2, icon: 'shuffle' },
+    { title: 'Recomendados', pageName: 'TabsPage',tabComponent: 'RecomendadosPage',index: 3, icon: 'shuffle' }
+  ];
+ 
+  constructor(public navCtrl: NavController) { }
+ 
+  openPage(page: PageInterface) {
+    let params = {};
+ 
+    // The index is equal to the order of our tabs inside tabs.ts
+    if (page.index) {
+      params = { tabIndex: page.index };
+    }
+ 
+    // The active child nav is our Tabs Navigation
+    if (this.nav.getActiveChildNav() && page.index != undefined) {
+      this.nav.getActiveChildNav().select(page.index);
+    } else {
+      // Tabs are not active, so reset the root page 
+      // In this case: moving to or from SpecialPage
+      this.nav.setRoot(page.pageName, params);
+    }
+  }
+ 
+  isActive(page: PageInterface) {
+    // Again the Tabs Navigation
+    let childNav = this.nav.getActiveChildNav();
+ 
+    if (childNav) {
+      if (childNav.getSelected() && childNav.getSelected().root === page.tabComponent) {
+        return 'primary';
+      }
+      return;
+    }
+ 
+    // Fallback needed when there is no active childnav (tabs not active)
+    if (this.nav.getActive() && this.nav.getActive().name === page.pageName) {
+      return 'primary';
+    }
+    return;
+  }
+ 
+}
